66b9d0550e208520db56812c8e87619129911518
Niyas Nazar <niyas28nazar@gmail.com>
2025-01-13 14:49:23 +0530

    push all modified and new  files

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app=express();
+const path=require("path")
 const env=require("dotenv")
 const db=require("./config/db");
+const userRouter=require("./routes/userRoutes")
 env.config()
 db();
 
  
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,'views/admin')]);
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/",userRouter);
 
 app.listen(parseInt(process.env.PORT),()=>{
 
