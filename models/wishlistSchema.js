86fcc921a68396c1ad833a8915f5d37cca32287d
Niyas Nazar <niyas28nazar@gmail.com>
2025-01-14 14:20:59 +0530

    model schema added

---
+++ models/wishlistSchema.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const {Schema}=mongoose;
+
+const WishlistSchema = new Schema({
+     
+    userId:{
+
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+
+    },
+    products:[{
+        productId:{
+            types:Schema.Types.ObjectId,
+            ref:"Product",
+            required:true,
+        },
+        addedOn:{
+            type:Date,
+            default:Date.now
+        }
+    }]
+
+})
+
+const Wishlist=mongoose.model("Wishlist",WishlistSchema);
+
+module.exports=Wishlist;
