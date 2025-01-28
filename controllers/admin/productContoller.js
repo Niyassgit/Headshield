2a084debf9ad9d7994a70d9e041b69d35ce41bf1
Niyas Nazar <niyas28nazar@gmail.com>
2025-01-28 21:42:31 +0530

    product edit page fully completed

--- controllers/admin/productContoller.js
+++ controllers/admin/productContoller.js
@@ -23,51 +23,51 @@ const getProductAddPage=async (req,res)=>{
 };
 
 const addProducts= async(req,res)=>{
-    try {
-        
-        const products=req.body;
-        const productExists=await Product.findOne({productName:products.productName,
+  try {
+      
+      const products=req.body;
+      const productExists=await Product.findOne({productName:products.productName,
 
-        });
-        if(!productExists){
-            const images=[];
-            if(req.files && req.files.length>0){
-                for(let i=0;i<req.files.length;i++){
-                    const originalImagePath =req.files[i].path;
-                    const resizedImagePath=path.join('public','uploads','product.images',req.files[i].filename);
-                    await sharp(originalImagePath).resize({width:440,height:440}).toFile(resizedImagePath);
-                    images.push(req.files[i].filename);
-        
-                }
-            }
-               const categoryId=await Category.findOne({name:products.category});
-               if(!categoryId){
-                return res.status(400).join("Invalid category name")
-               }
-               const newProduct =new Product({
-                productName:products.productName,
-                description:products.description,
-                brand:products.brand,
-                category:categoryId._id,
-                regularPrice:products.regularPrice,
-                salePrice:products.salePrice,
-                createdOn:new Date(),
-                quantity:products.quantity,
-                size:products.size,
-                color:products.color,
-                productImage:images,
-                status:"Available",
-               });
+      });
+      if(!productExists){
+          const images=[];
+          if(req.files && req.files.length>0){
+              for(let i=0;i<req.files.length;i++){
+                  const originalImagePath =req.files[i].path;
+                  const resizedImagePath=path.join('public','uploads','product.images',req.files[i].filename);
+                  await sharp(originalImagePath).resize({width:440,height:440}).toFile(resizedImagePath);
+                  images.push(req.files[i].filename);
+      
+              }
+          }
+             const categoryId=await Category.findOne({name:products.category});
+             if(!categoryId){
+              return res.status(400).join("Invalid category name")
+             }
+             const newProduct =new Product({
+              productName:products.productName,
+              description:products.description,
+              brand:products.brand,
+              category:categoryId._id,
+              regularPrice:products.regularPrice,
+              salePrice:products.salePrice,
+              createdOn:new Date(),
+              quantity:products.quantity,
+              size:products.size,
+              color:products.color,
+              productImage:images,
+              status:"Available",
+             });
 
-               await newProduct.save();
-               return res.redirect("/admin/addProducts");
-        }else{
-            return res.status(400).json("Product already exist,please try with another name");
-        }
-    } catch (error) {
-        console.error("Error saving product",error);
-        return res.redirect("/admin/admin-pageerror");
-    }
+             await newProduct.save();
+             return res.redirect("/admin/addProducts");
+      }else{
+          return res.status(400).json("Product already exist,please try with another name");
+      }
+  } catch (error) {
+      console.error("Error saving product",error);
+      return res.redirect("/admin/admin-pageerror");
+  }
 }
 const getAllProducts = async (req, res) => {
     try {
@@ -162,7 +162,7 @@ const getEditProduct=async(req,res)=>{
 };
 
 const editProduct=async(req,res)=>{
-
+  
   try {
 
     const id=req.params.id;
@@ -237,6 +237,8 @@ const deleteSingleImage = async (req, res) => {
       res.status(500).send({ status: false, error: "Server error occurred" });
   }
 };
+
+
 
 
   
